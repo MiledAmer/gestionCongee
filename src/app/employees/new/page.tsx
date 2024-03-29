5cbbfbb5b266f02517c4139182948f71606a4d09
MiledAmer <midou.slm8@gmail.com>
2024-03-29 16:31:27 +0100

    finish topbar

---
+++ src/app/employees/new/page.tsx
@@ -0,0 +1,6 @@
+
+export default function NewEmplyee() {
+  return (
+    <div>NewEmplyee</div>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import ResponsiveAppBar from "../components/topbar";
+
+export default function Home() {
+  return (
+    <ResponsiveAppBar/>
+  )
+}
